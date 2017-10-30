999b51a3b5104333478d5016653863a3c566f3c6
Nat! <nat@codeon.de>
2017-10-30 13:38:06 +0100

    fix wrong output in usage

--- src/mulle-bootstrap-fetch.sh
+++ src/mulle-bootstrap-fetch.sh
@@ -83,7 +83,7 @@ EOF
    local  repositories
 
    repositories="`all_repository_names`"
-   if [ -z "${repositories}" ]
+   if [ ! -z "${repositories}" ]
    then
       echo "Currently available repositories are:"
       echo "${repositories}" | sed 's/^/   /'
